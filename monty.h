66121de02112107616e71558b41a8bbede788631
Kevin Koech <kevinkoech@proton.me>
2023-05-25 22:12:31 +0300

    added pint prototype

--- monty.h
+++ monty.h
@@ -47,6 +47,7 @@ void pall(stack_t **stack);
 void read_file(const char *test_file);
 void interpret(stack_t **stack, char *line, unsigned int line_number);
 void instruct_error(char *instruction, unsigned int line);
+void pint(stack_t **stack, unsigned int line_number);
 
 #endif /* MONTY_H */
 
