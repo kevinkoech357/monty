68cabefa9d5cc5da0e2206f040792a3e0cf8f01e
Kevin Koech <kevinkoech@proton.me>
2023-05-25 15:46:56 +0300

    added push command function

---
+++ push_pall.c
@@ -0,0 +1,52 @@
+#include "monty.h"
+
+/**
+* push - pushes new elements into stack_t
+*
+* @stack: pointer to pointer to stack 
+*
+* @value: new data to be added to stack_t
+*
+* @line_number: line number where the instruction appears
+*
+* Return: void
+*
+*/
+
+void push(stack_t **stack, int value, size_t line_number)
+{
+	stack_t *new_node;
+
+	new_node = malloc(sizeof(stack_t));
+
+	if (new_node == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+
+	new_node->n = value;
+	new_node->prev = NULL;
+	new_node->next = *stack;
+
+	if (*stack != NULL)
+	{
+		(*stack)->prev = new_node;
+	}
+
+	*stack = new_node;
+}
+
+/**
+* pall - prints all elements in stack_t
+*
+* @stack: pointer to pointer to stack
+*
+* Return: void
+*
+*/
+
+void pall(stack_t **stack)
+{
+
+}
