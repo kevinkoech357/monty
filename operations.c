6c8c58b83bde4f79f657fcb37b75809c9ba160fa
Kevin Koech <kevinkoech@proton.me>
2023-05-26 21:54:42 +0300

    added add functionality

--- operations.c
+++ operations.c
@@ -96,3 +96,31 @@ void nop(stack_t **stack, unsigned int line_number)
 	(void)stack;
 	(void)line_number;
 }
+
+/**
+* add - adds stack elements
+*
+* @stack: pointer to pointer to stack
+*
+* @line_number: line number with add instruction
+*
+* Return: void
+*/
+
+void add(stack_t **stack, unsigned int line_number)
+{
+	stack_t *tempo;
+
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%u: can't add, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	tempo = *stack;
+	(*stack)->next->n += (*stack)->n;
+	*stack = (*stack)->next;
+	(*stack)->prev = NULL;
+	free(tempo);
+}
+
